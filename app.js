7f7c113a7f9dd58fcc7f3d7cdccd0fb7cb863439
Jro <bobo999bobo@outlook.com>
2018-10-09 15:35:10 +0800

    Add api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const api = require("./routes/api");
 
 var app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(api);
 
 module.exports = app;
---
+++ routes/api.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const request = require("request-promise-native")
+const router = express.Router()
+
+
+router.post("/api/getData",(req,res)=>{
+  ;(async ()=>{
+    const {ACCOUNT_KEY:accountKey} = process.env
+    let endpoint
+    const options = {}
+    if(accountKey===undefined){
+      //Use public endpoint
+      endpoint = "https://arrivelah.herokuapp.com/?id="
+    }else{
+      endpoint = 'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode='
+      options.headers = {
+        AccountKey: accountKey
+      }
+    }
+    const busStops = ["16991", "17191", "17129", "17121"]
+    const results = await Promise.all(busStops.map(id=>request(`${endpoint}${id}`,options)))
+    return res.json(results.map(JSON.parse))
+  })()
+  .catch(err=>{
+    let code = err.code || 500
+    res.status(code).end(err.toString().replace("Error: ",""))
+  })
+})
+
+module.exports = router
